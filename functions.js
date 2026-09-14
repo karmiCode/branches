ed80c1b29cf3ac2266ecb042c3d218da37da8539
karmiCode <a0527141988@gmail.com>
2026-09-14 12:35:11 +0300

    change functions to numbers

---
+++ functions.js
@@ -0,0 +1,11 @@
+const func1 = (num1,num2) => {
+    console.log(num1*num2)
+}
+
+const func2 = (num1,num2) => {
+    console.log(num1+num2)
+}
+
+const func3 = (num1,num2) => {
+    console.log(num1-num2)
+}
